a030572384cb6abe5823f41fdeaa629c58d24335
Ali Babolhavaeji <40876922+Alibhji@users.noreply.github.com>
2020-06-08 15:35:31 -0400

    Train script is added.

--- lib/model/train_val.py
+++ lib/model/train_val.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import time
 import torch
+import copy
 
 
 def loss_function (output ,target,criterion ):
@@ -34,7 +35,7 @@ def get_lr(optimizer):
     return lr_
 
 
-def train_val(model, data_loader, epoch, device, optimizer, criterion, config, mode='train',logger=None , history=None ,writer =None):
+def train_val(model, data_loader, epoch, device, optimizer, criterion, config, mode='train',logger=None , history=None ,this_expr =None):
     
     def Logger(msg, logger=logger):
         if logger is not None:
@@ -57,6 +58,7 @@ def train_val(model, data_loader, epoch, device, optimizer, criterion, config, m
 
 
 
+
         optimizer = lr_scheduler(optimizer, epoch,
                                      lr_decay=cfg['train']['lr']['lr_decay'],
                                      lr_decay_epoch=cfg['train']['lr']['lr_decay_epoch'] ,
@@ -65,10 +67,10 @@ def train_val(model, data_loader, epoch, device, optimizer, criterion, config, m
         lr__ = get_lr(optimizer)
 
 
-        # print("-------------------LR----->", get_lr(optimizer))
     elif mode == 'val':
         model.eval()
-    # Logger(f" Mode [mode] is set to: {mode}")
+        
+    #this_expr.print_log(f" Mode [mode] is set to: {mode}" , print_=False)
 
 
     for i, data in enumerate(r, 0):
@@ -92,35 +94,26 @@ def train_val(model, data_loader, epoch, device, optimizer, criterion, config, m
             loss.backward()
             optimizer.step()
 
-            # loss_value = copy.deepcopy(loss.cpu().detach().numpy())
-            # L2D = copy.deepcopy(loss_BEV.cpu().detach().numpy())
-            # L3D = copy.deepcopy(loss_Pts_3D.cpu().detach().numpy())
+            loss_value = copy.deepcopy(loss.cpu().detach().numpy())
+            running_loss += loss.item() / len(data_loader)
 
-            # running_loss += loss.item() / len(data_loader)
-            # running_BEV += loss_BEV.item() / len(data_loader)
-            # running_pts_3D += loss_Pts_3D.item() / len(data_loader)
+            time_elapsed_batch = time.time() - since
+            time_elapsed = time.time() - ep_since
+            dis_msg =\
+                f"[T] loss: {loss_value:2.5f} total_loss: {running_loss:02.6f} " \
+                      f"[lr: {lr__:0.07f}] [Time {time_elapsed_batch: 03.3f} {time_elapsed: 03.3f}]"
+            r.set_description(dis_msg)
+            this_expr.writer.add_scalar('training_batch_loss',
+                              loss_value,
+                              epoch * len(data_loader) + i)
 
-            # time_elapsed_batch = time.time() - since
-            # time_elapsed = time.time() - ep_since
-            # dis_msg =\
-                # f"[T] loss: {loss_value:2.5f} total_loss: {running_loss:02.6f} (L2D:{running_BEV:02.6f} " \
-                      # f"L3D:{running_pts_3D:02.6f}) (lr: {lr__:0.07f} (time {time_elapsed_batch: 03.3f} {time_elapsed: 03.3f})"
-            # r.set_description(dis_msg)
-            # writer.add_scalar('training_batch_loss',
-                              # loss_value,
-                              # epoch * len(data_loader) + i)
+            if history is not None:
+                history.loc[epoch * len(data_loader) + i, 'train_loss_value'] = loss_value
+                history.loc[epoch * len(data_loader) + i, 'train_running_loss'] = running_loss
 
-            # if history is not None:
-                # history.loc[epoch * len(data_loader) + i, 'train_loss_value'] = loss_value
-                # history.loc[epoch * len(data_loader) + i, 'train_L2D'] = L2D
-                # history.loc[epoch * len(data_loader) + i, 'train_L3D'] = L3D
 
-                # history.loc[epoch * len(data_loader) + i, 'train_running_loss'] = running_loss
-                # history.loc[epoch * len(data_loader) + i, 'train_running_BEV'] = running_BEV
-                # history.loc[epoch * len(data_loader) + i, 'train_running_pts_3D'] = running_pts_3D
-
-            # if(i==20):
-            #     break
+            if(i==4):
+                break
 
         else:
 
@@ -128,70 +121,55 @@ def train_val(model, data_loader, epoch, device, optimizer, criterion, config, m
             
             loss = loss_function(output ,meta['stalled'] ,criterion)
 
-            # loss, loss_BEV, loss_Pts_3D = loss_function(output, mask_bev, cfg, criterion, device , meta_data = meta_data)
 
-            # loss_value = copy.deepcopy(loss.cpu().detach().numpy())
-            # L2D = copy.deepcopy(loss_BEV.cpu().detach().numpy())
-            # L3D = copy.deepcopy(loss_Pts_3D.cpu().detach().numpy())
+            loss_value = copy.deepcopy(loss.cpu().detach().numpy())
+            running_loss += loss.item() / len(data_loader)
 
-            # running_loss += loss.item() / len(data_loader)
-            # running_BEV += loss_BEV.item() / len(data_loader)
-            # running_pts_3D += loss_Pts_3D.item() / len(data_loader)
+            time_elapsed_batch = time.time() - since
+            time_elapsed = time.time() - ep_since
+            dis_msg =\
+                f"[E] loss: {loss_value:2.5f} total_loss: {running_loss:02.6f} " \
+                      f"[Time {time_elapsed_batch: 03.3f} {time_elapsed: 03.3f}]"
+            r.set_description(dis_msg)
 
-            # time_elapsed_batch = time.time() - since
-            # time_elapsed = time.time() - ep_since
-            # dis_msg =\
-                # f"[E] loss: {loss_value:2.5f} total_loss: {running_loss:02.6f} " \
-                # f"(L2D:{running_BEV:02.6f} L3D:{running_pts_3D:02.6f})" \
-                # f" (time {time_elapsed_batch: 03.3f} {time_elapsed: 03.3f})"
-            # r.set_description(dis_msg)
+            this_expr.writer.add_scalar('eval_batch_loss',
+                              loss_value,
+                              epoch * len(data_loader) + i)
 
-            # writer.add_scalar('eval_batch_loss',
-                              # loss_value,
-                              # epoch * len(data_loader) + i)
-
-            # if history is not None:
-                # history.loc[epoch * len(data_loader) + i, 'eval_loss_value'] = loss_value
-                # history.loc[epoch * len(data_loader) + i, 'eval_L2D'] = L2D
-                # history.loc[epoch * len(data_loader) + i, 'eval_L3D'] = L3D
-
-                # history.loc[epoch * len(data_loader) + i, 'eval_running_loss'] = running_loss
-                # history.loc[epoch * len(data_loader) + i, 'eval_running_BEV'] = running_BEV
-                # history.loc[epoch * len(data_loader) + i, 'eval_running_pts_3D'] = running_pts_3D
-
-            # ES_BEV = output[0].cpu().detach()
-            # meta_data['BEV'] = BEV_2d.cpu().detach()
-            # meta_data['front'] =bboxs_f.cpu().detach()
+            if history is not None:
+                history.loc[epoch * len(data_loader) + i, 'eval_loss_value'] = loss_value
+                history.loc[epoch * len(data_loader) + i, 'eval_running_loss'] = running_loss
+            
+            if(i==4):
+                break
             
 
     if mode == 'train':
-        Logger(dis_msg)
+        this_expr.print_log(dis_msg , print_=False)
 
         if history is not None:
             history.loc[epoch, 'train_total_loss'] = running_loss
             history.loc[epoch, 'lr'] = lr__
-            history.loc[epoch, 'train_BEV_loss'] = running_BEV
-            history.loc[epoch, 'train_pts_3D_loss'] = running_pts_3D
             history.loc[epoch, 'train_time'] = time.time() - ep_since
 
 
-        if writer is not None:
-            writer.add_scalar('training_total_loss',
-                          loss_value,
+
+        this_expr.writer.add_scalar('training_total_loss',
+                      loss_value,
+                      epoch * len(data_loader) + i)
+        this_expr.writer.add_scalar('lr',
+                          lr__,
                           epoch * len(data_loader) + i)
-            writer.add_scalar('lr',
-                              lr__,
-                              epoch * len(data_loader) + i)
 
     else:
-    
+        this_expr.print_log(dis_msg , print_=False)
         pass
-        # if history is not None:
-            # history.loc[epoch, 'eval_total_loss'] = running_loss
+        if history is not None:
+            history.loc[epoch, 'eval_total_loss'] = running_loss
             # history.loc[epoch, 'eval_BEV_loss'] = running_BEV
             # history.loc[epoch, 'eval_pts_3D_loss'] = running_pts_3D
 
-            # history.loc[epoch, 'eval_time'] = time.time() - ep_since
+            history.loc[epoch, 'eval_time'] = time.time() - ep_since
 
         # with open("evaltion_Pack.pandas", 'wb') as handle:
             # pickle.dump(evaltion_Pack, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- script/train.py
+++ script/train.py
@@ -2,10 +2,13 @@
 
 
 import sys
-import torch
 import yaml
 import os
+import gc
+import pandas as pd
 
+import torch
+import torch.nn as nn
 package_path = '..'
 # package_path = '../../script2/Result/nuScenes/exp3/sources/'
 if not package_path in sys.path:
@@ -17,29 +20,103 @@ from lib import my_deep_clag_loss
 from torch.utils.data import DataLoader
 from lib import  train_val
 from lib import create_new_experiment
+import pickle
 
 config = './config.yml'
 with open (config , 'rb') as f:
     config = yaml.load(f ,Loader=yaml.FullLoader)
 
+# get a backup from important file in the project
 this_expr = create_new_experiment(config)
 this_expr.copyanything(os.path.basename(__file__) ,os.path.join(this_expr.sourefiles_path,'script') )
 this_expr.copyanything(os.path.basename('./config.yml') ,os.path.join(this_expr.sourefiles_path,'script') )
 this_expr.copyanything(os.path.abspath('../lib'), os.path.join(this_expr.sourefiles_path, 'lib'))
 
 # create train and validation dataset
-train_Dataset = ClogLossDataset_from_compressed_data(config)
+fold = 0
+train_dataset = ClogLossDataset_from_compressed_data(config , split='train' ,fold = fold )
+val_dataset   = ClogLossDataset_from_compressed_data(config , split='val' ,fold = fold)
 
+this_expr.print_log(f"Trin dataset: {len(train_dataset)} objects")
+this_expr.print_log(f"Val dataset : {len(val_dataset)} objects")
 
-train_loader = DataLoader(train_Dataset, batch_size=2,
-                          num_workers=0, shuffle=True)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+this_expr.print_log(f"Device      : {device}")
+
+train_loader = DataLoader(train_dataset, batch_size=config['train']['trainBatchSize'],
+                          num_workers=config['train']['num_Workers'], shuffle=True , pin_memory=True)
+
+val_loader = DataLoader(val_dataset, batch_size=config['train']['valBatchSize'], num_workers=config['train']['num_Workers'],
+                        shuffle=True ,pin_memory=True)
+                        
+                        
+                        
 
 vid_tensor , meta = next(iter(train_loader))
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = my_deep_clag_loss()
 model.to(device)
+
+if config['model']['load']['flag']:
+    state_dict = torch.load(config['model']['load']['path'])
+    model.load_state_dict(state_dict)
+    this_expr.print_log(f"[info] Model is loaded. [from {config['model']['load']['path']}]")
+
+    
+    
+    
 # inputs = torch.randn((2, 3, 200, 150, 150)).to(device)
 
 vid_tensor = vid_tensor.to(device)
 print(model(vid_tensor).shape)
+
+
+optimizer = torch.optim.Adam(model.parameters(), lr=config['train']['lr']['init'], weight_decay=1e-5)
+# criterion = nn.MSELoss()
+criterion = nn.BCELoss()
+
+
+best_loss = 1e+10
+history = pd.DataFrame()
+
+for epoch in range(config['train']['startEpoch'], config['train']['endEpoch']):  # loop over the dataset multiple times
+    this_expr.print_log(f"=======================  Epoch {epoch} / {config['train']['endEpoch']}  =======================")
+    gc.collect()
+
+
+    _, history             =   train_val(model, 
+                                         train_loader, 
+                                         epoch, device, 
+                                         optimizer, 
+                                         criterion, 
+                                         config, 
+                                         mode='train',
+                                         history =history,
+                                         this_expr = this_expr )
+    
+    
+    curr_val_loss, history =   train_val(model, 
+                                         val_loader, 
+                                         epoch, device, 
+                                         optimizer, 
+                                         criterion, 
+                                         config, 
+                                         mode='val',
+                                         history =history,
+                                         this_expr = this_expr)
+                                         
+    with open(os.path.join(this_expr.model_checkpoints_path, f'history.pkl'), 'wb') as handle:
+        pickle.dump(history, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+    this_expr.print_log('current_val_loss: ', f"{curr_val_loss}")
+    
+    if curr_val_loss < best_loss:
+        model_save_format = f"model_E{epoch:03d}_Loss{curr_val_loss:.6f}.pt"
+        # torch.save(model.state_dict(), os.path.join(experiment_name ,f"./model_E{epoch:03d}_Loss{curr_val_loss:.6f}.pt"))
+        torch.save(model.state_dict(), os.path.join(this_expr.model_checkpoints_path, model_save_format))
+        # print (f"./model_E{epoch:03d}_Loss{curr_val_loss:.6f}.pt  is saved.")
+        this_expr.print_log(os.path.join(this_expr.model_checkpoints_path, model_save_format) + " is saved.")
+
+        best_loss = curr_val_loss
+        with open(os.path.join(this_expr.model_checkpoints_path, f'history_best_model.pkl'), 'wb') as handle:
+            pickle.dump(history, handle, protocol = pickle.HIGHEST_PROTOCOL)
